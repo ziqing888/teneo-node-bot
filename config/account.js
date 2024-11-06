c8f1c1e4eb1003f5debd52b3ff982d708af2a24a
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-06 15:03:43 +0800

    Create account.js

---
+++ config/account.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    email: "account1@example.com",
+    password: "password1"
+  },
+  {
+    email: "account2@example.com",
+    password: "password2"
+  },
+  // 根据需要添加更多账户
+];

00c67ffdf5f778d38bb3981fbf8b6e7d42d0fb4a
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-06 15:45:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-const axios = require('axios');
-const logger = require('./src/logger');
-const { connectWebSocket } = require('./src/websocketManager');
-const accounts = require('./config/account.js');
+// index.js 文件，项目的主要入口
 
-// 原有的硬编码授权和 API 密钥
-const authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag";
-const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag";
+// 导入日志管理模块
+const { displayHeader, log_info } = require('./src/logger');
+const accounts = require('./config/account');
+const startBot = require('./src/bot');
 
-let userIds = [];
+// 显示项目标题（欢迎信息）
+displayHeader(); 
 
-async function getUserId(index) {
-  const loginUrl = "https://ikknngrgxuxgjhplbpey.supabase.co/auth/v1/token?grant_type=password";
-
-  try {
-    const response = await axios.post(loginUrl, {
-      email: accounts[index].email,
-      password: accounts[index].password
-    }, {
-      headers: {
-        'Authorization': authorization,
-        'apikey': apikey
-      }
-    });
-
-    userIds[index] = response.data.user.id;
-    logger.success(`Account ${index + 1} User ID Acquired: ${userIds[index]}`);
-    connectWebSocket(index, userIds[index]);
-  } catch (error) {
-    logger.error(`Error fetching User ID for Account ${index + 1}: ${error.response ? error.response.data : error.message}`);
-  }
-}
-
-function startBot() {
-  for (let i = 0; i < accounts.length; i++) {
-    getUserId(i);
-  }
-}
-
-startBot();
+// 启动机器人，使用配置文件中的所有账户
+accounts.forEach(account => {
+  log_info(`正在启动账户 ${account.email}...`);
+  startBot(account);
+});

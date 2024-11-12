bc2638600e606b871bb6f59efc30c03bcefb76ec
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-12 11:11:37 +0800

    Create accounts_tmp.js

---
+++ accounts/accounts_tmp.js
@@ -0,0 +1,26 @@
+/**
+ * 账户列表文件
+ * 请按照以下格式编写您的账户
+ * export const accountLists = [
+ *       {
+ *           email: "您的邮箱",
+ *           password: "您的密码",
+ *       },
+ *       {
+ *           email: "您的邮箱",
+ *           password: "您的密码",
+ *       },
+ * ];
+ *
+ */
+
+export const accountLists = [
+    {
+        email: "您的邮箱",
+        password: "您的密码",
+    },
+    {
+        email: "您的邮箱",
+        password: "您的密码",
+    },
+];
